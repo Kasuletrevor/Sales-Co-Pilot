b5c047a9413e8937889b585a14cfd57fb1f7e13a
Kasule John Trevor <82664582+Kasuletrevor@users.noreply.github.com>
2025-04-18 14:06:28 +0300

    add main.py to initialize environment and import necessary modules

---
+++ main.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from tools import search_tool, wiki_tool, save_tool
+
+load_dotenv()
